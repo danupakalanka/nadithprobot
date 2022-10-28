f1dcc320989ba866296aa8953dd87853c607674d
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-10-29 01:56:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const {
 	default: makeWASocket,
-	useMultiFileAuthState,
+	useSingleFileAuthState,
 	DisconnectReason,
 	getContentType,
 	jidDecode
@@ -10,7 +10,7 @@ const fs = require('fs')
 const P = require('pino')
 const qrcode = require('qrcode-terminal')
 const util = require('util')
-const { state, saveState } = useMultiFileAuthState('./session.json')
+const { state, saveState } = useSingleFileAuthState('./session.json')
 const config = require('./config')
 const prefix = '/'
 const owner = ['94761327688']
