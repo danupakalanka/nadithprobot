7d10dccb2abee6e52769954857f9f6b2709f7629
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-10-29 01:14:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const connectToWA = () => {
 					break
 
 					case 'dsub' :  {
-						conn.sendMessage(from, { document : { url : 'https://nadithprolinkgen.nadithdhanula.repl.co/www.1TamilMV.vin%20-%20PAAGAL%20(2021)%20Telugu%20_%20-%20720p%20-%20HEVC%20-%20(DD5.1%20-%20192Kbps%20%20AAC%202.0)%20-%20950MB%20-%20ESub.mkv' }, fileName : 'movie', mimetype: 'video/x-matroska', caption : config.CAPTION }, { quoted : mek })}	
+						conn.sendMessage(from, { document : { url : 'http://admin.nadith.pro/www.1TamilMV.vin%20-%20PAAGAL%20(2021)%20Telugu%20_%20-%20720p%20-%20HEVC%20-%20(DD5.1%20-%20192Kbps%20%20AAC%202.0)%20-%20950MB%20-%20ESub.mkv' }, fileName : 'movie', mimetype: 'video/x-matroska', caption : config.CAPTION }, { quoted : mek })}	
 						 break
 
 
