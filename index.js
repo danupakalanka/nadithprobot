30177ac56640f89a82725a0227a1c345882c4ad5
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-10-30 19:43:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,12 +18,9 @@ const yts = require('yt-search')
 const axios = require('axios')
 const apk_link = require('./lib/playstore')
 
-const { version, isLatest } = fetchLatestBaileysVersion()
-	console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
 
 const connectToWA = () => {
 	const conn =  makeWASocket({
-		version,
 		logger,
 		printQRInTerminal: true,
 		auth: {
